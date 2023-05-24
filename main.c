9c4a18ad63ea5ae27fa607e87b2d8285cc435c97
HodaSalim <hi@hodasalim.com>
2023-05-24 11:54:24 +0300

    main.c file

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - entry point.
+ *@argv: array of strings.
+ *@argc: argc number of arguments.
+ *@env: environment varible.
+ */
+
+int main(int argc, char **argv, char **env)
+{
+
+	if (argc == 1)
+		init(argv, env);
+	return (0);
+}
+
+
+
